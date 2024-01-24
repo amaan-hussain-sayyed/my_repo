dc74062cf65c90da104d81838490c8dfaf8f5769
shavez-79a <amaan.hussain.sayyed@gmail.com>
2024-01-25 00:57:51 +0530

    deleting

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.set("view engine", "ejs")
 app.get("/", (req, res) => {
     res.render("index")
 })
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT,"0.0.0.0",() => {
     console.log("server started to http://localhost:" + process.env.PORT)
 })
